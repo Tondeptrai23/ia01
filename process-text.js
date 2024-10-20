95cc9a583edbfa643db79fb91f132248edbb0de4
Tondeptrai23 <22120375@student.hcmus.edu.vn>
2024-10-20 10:10:15 +0700

    Now can change sample text style

--- process-text.js
+++ process-text.js
@@ -1,6 +1,48 @@
 $(document).ready(function () {
     $("#style-btn").click(function () {
-        console.log("clicked");
         $(".style-drop-down").toggle();
+    });
+
+    const style = {
+        color: "",
+        backgroundColor: "",
+        isBold: false,
+        isItalic: false,
+        isUnderline: false,
+    };
+
+    const changeStyle = function () {
+        $("#sample-text").css({
+            color: style.color,
+            backgroundColor: style.backgroundColor,
+            fontWeight: style.isBold ? "bold" : "normal",
+            fontStyle: style.isItalic ? "italic" : "normal",
+            textDecoration: style.isUnderline ? "underline" : "none",
+        });
+    };
+
+    $("#color-ipn").on("input", function () {
+        style.color = $(this).val();
+        changeStyle();
+    });
+
+    $("#background-ipn").on("input", function () {
+        style.backgroundColor = $(this).val();
+        changeStyle();
+    });
+
+    $("#bold-ipn").on("click", function () {
+        style.isBold = !style.isBold;
+        changeStyle();
+    });
+
+    $("#italic-ipn").on("click", function () {
+        style.isItalic = !style.isItalic;
+        changeStyle();
+    });
+
+    $("#underline-ipn").on("click", function () {
+        style.isUnderline = !style.isUnderline;
+        changeStyle();
     });
 });
