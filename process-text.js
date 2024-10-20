d64a8c8b87a556eb82195bd51ddac29d0888d68a
Tondeptrai23 <22120375@student.hcmus.edu.vn>
2024-10-20 11:14:39 +0700

    Add highlight, delete, reset behavior

--- process-text.js
+++ process-text.js
@@ -11,8 +11,10 @@ $(document).ready(function () {
         isUnderline: false,
     };
 
-    const changeStyle = function () {
-        $("#sample-text").css({
+    let isHighlighting = false;
+
+    const changeStyle = function (selector) {
+        $(selector).css({
             color: style.color,
             backgroundColor: style.backgroundColor,
             fontWeight: style.isBold ? "bold" : "normal",
@@ -21,28 +23,84 @@ $(document).ready(function () {
         });
     };
 
+    const originalText = $("#main-text").text();
+
+    const handleStyleChange = function () {
+        changeStyle("#sample-text");
+
+        const $mainText = $("#main-text");
+        const text = $mainText.text();
+        if (isHighlighting) {
+            const regex = /a/g;
+
+            const newText = text.replace(regex, function (match) {
+                return `<span>${match}</span>`;
+            });
+
+            $mainText.html(newText);
+
+            changeStyle("#main-text span");
+        } else {
+            $mainText.html(
+                text.replace(/<span>/g, "").replace(/<\/span>/g, "")
+            );
+        }
+    };
+
+    $("#highlight-btn").click(function () {
+        isHighlighting = !isHighlighting;
+
+        handleStyleChange();
+    });
+
+    $("#reset-btn").click(function () {
+        $("#main-text").text(originalText);
+        style.color = "";
+        style.backgroundColor = "";
+        style.isBold = false;
+        style.isItalic = false;
+        style.isUnderline = false;
+
+        $("#color-ipn").val("");
+        $("#background-ipn").val("");
+        $("#bold-ipn").prop("checked", false);
+        $("#italic-ipn").prop("checked", false);
+        $("#underline-ipn").prop("checked", false);
+
+        handleStyleChange();
+    });
+
+    $("#delete-btn").click(function () {
+        const text = $("#main-text").text();
+        const newText = text.replace(/a/g, "");
+
+        $("#main-text").text(newText);
+
+        handleStyleChange();
+    });
+
     $("#color-ipn").on("input", function () {
         style.color = $(this).val();
-        changeStyle();
+        handleStyleChange();
     });
 
     $("#background-ipn").on("input", function () {
         style.backgroundColor = $(this).val();
-        changeStyle();
+        handleStyleChange();
     });
 
     $("#bold-ipn").on("click", function () {
         style.isBold = !style.isBold;
-        changeStyle();
+        handleStyleChange();
     });
 
     $("#italic-ipn").on("click", function () {
         style.isItalic = !style.isItalic;
-        changeStyle();
+        handleStyleChange();
     });
 
     $("#underline-ipn").on("click", function () {
         style.isUnderline = !style.isUnderline;
-        changeStyle();
+        handleStyleChange();
     });
 });
